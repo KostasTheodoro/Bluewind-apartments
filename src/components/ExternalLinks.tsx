e95b7a4a1ab97af455c7203fe3fb72a9ff45f83e
KostasTheodoro <98785109+KostasTheodoro@users.noreply.github.com>
2025-02-17 19:39:15 +0200

    fixed layout for externallinks

--- src/components/ExternalLinks.tsx
+++ src/components/ExternalLinks.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 const ExternalLinks = () => {
   return (
-    <div className="flex justify-end py-12 items-center">
+    <div className="flex flex-col lg:flex-row justify-end py-12 items-center">
       {/* Booking Button */}
       <div className="p-4">
         <Link
           href="https://www.booking.com/hotel/gr/bluewind-apartments-near-the-sea"
           target="_blank"
-          className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-xl font-medium
+          className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-lg font-medium
           hover:border-4 hover:font-bold hover:bg-neutral-lightGray hover:border-primary-coral hover:scale-95 hover:transition-all hover:ease-in-out
           focus:outline-none focus:ring-0 focus:ring-primary-coral focus:ring-opacity-50 focus:ring-offset-2 transition-all duration-300 ease-in-out"
         >
@@ -25,15 +25,16 @@ const ExternalLinks = () => {
           </div>
         </Link>
       </div>
-      {/* Separator Line */}
-      <div className="border-l-2 border-primary h-32 p-4"></div>
+      {/* Separator: vertical on lg, horizontal on smaller screens */}
+      <div className="hidden lg:block border-l-2 border-primary h-32 p-4"></div>
+      <div className="block lg:hidden border-t-2 border-primary w-full p-4"></div>
       {/* Airbnb Links */}
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="flex flex-col space-y-4">
           <Link
             href="https://www.airbnb.gr/rooms/42484053?source_impression_id=p3_1737825240_P3C4R2jrZDklesW-"
             target="_blank"
-            className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-xl font-medium
+            className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-lg font-medium
               hover:border-4 hover:border-primary-coral hover:text-primary hover:font-bold hover:bg-neutral-lightGray hover:scale-95 hover:transition-all hover:ease-in-out
               focus:outline-none focus:ring-0 focus:ring-primary-coral focus:ring-opacity-50 focus:ring-offset-2 transition-all duration-300 ease-in-out"
           >
@@ -42,7 +43,7 @@ const ExternalLinks = () => {
               <Image
                 src="/images/airbnblogo.svg"
                 alt="airbnb logo"
-                width={70}
+                width={60}
                 height={40}
                 className="ml-4"
               />
@@ -51,7 +52,7 @@ const ExternalLinks = () => {
           <Link
             href="https://www.airbnb.gr/rooms/42777596?_set_bev_on_new_domain=1621165347_ZTVkMDM5YzZlMTU3&source_impression_id=p3_1621170584_CcF%2Fa1X3vFl2ID62&guests=1&adults=1"
             target="_blank"
-            className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-xl font-medium
+            className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-lg font-medium
               hover:border-4 hover:border-primary-coral hover:text-primary hover:font-bold hover:bg-neutral-lightGray hover:scale-95 hover:transition-all hover:ease-in-out
               focus:outline-none focus:ring-0 focus:ring-primary-coral focus:ring-opacity-50 focus:ring-offset-2 transition-all duration-300 ease-in-out"
           >
@@ -60,7 +61,7 @@ const ExternalLinks = () => {
               <Image
                 src="/images/airbnblogo.svg"
                 alt="airbnb logo"
-                width={70}
+                width={60}
                 height={40}
                 className="ml-4"
               />
@@ -71,7 +72,7 @@ const ExternalLinks = () => {
           <Link
             href="https://www.airbnb.gr/rooms/42659201?source_impression_id=p3_1737825305_P39x8L5HZgonR4Kn"
             target="_blank"
-            className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-xl font-medium
+            className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-lg font-medium
               hover:border-4 hover:border-primary-coral hover:text-primary hover:font-bold hover:bg-neutral-lightGray hover:scale-95 hover:transition-all hover:ease-in-out
               focus:outline-none focus:ring-0 focus:ring-primary-coral focus:ring-opacity-50 focus:ring-offset-2 transition-all duration-300 ease-in-out"
           >
@@ -80,7 +81,7 @@ const ExternalLinks = () => {
               <Image
                 src="/images/airbnblogo.svg"
                 alt="airbnb logo"
-                width={70}
+                width={60}
                 height={40}
                 className="ml-4"
               />
@@ -89,7 +90,7 @@ const ExternalLinks = () => {
           <Link
             href="https://www.airbnb.com.mt/rooms/42776701?source_impression_id=p3_1737825333_P3zpllITeIVmLpa3"
             target="_blank"
-            className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-xl font-medium
+            className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-lg font-medium
               hover:border-4 hover:border-primary-coral hover:text-primary hover:font-bold hover:bg-neutral-lightGray hover:scale-95 hover:transition-all hover:ease-in-out
               focus:outline-none focus:ring-0 focus:ring-primary-coral focus:ring-opacity-50 focus:ring-offset-2 transition-all duration-300 ease-in-out"
           >
@@ -98,9 +99,9 @@ const ExternalLinks = () => {
               <Image
                 src="/images/airbnblogo.svg"
                 alt="airbnb logo"
-                width={70}
+                width={60}
                 height={40}
-                className="ml-4"
+                className="ml-2"
               />
             </div>
           </Link>
