e19977aac051a56538cc1813c5cf297f987e05a8
KostasTheodoro <98785109+KostasTheodoro@users.noreply.github.com>
2025-05-07 13:57:58 +0300

    externallinks layout fix

--- src/components/ExternalLinks.tsx
+++ src/components/ExternalLinks.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 
+const buttonBaseStyles =
+  "group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-lg font-medium box-border " +
+  "hover:border-primary-coral hover:font-semibold hover:bg-neutral-lightGray hover:ring-2 hover:ring-primary-coral " +
+  "hover:scale-95 transition-all duration-300 ease-in-out focus:outline-none focus:ring-0 focus:ring-offset-2";
+
+const logoHoverStyles =
+  "transition-transform duration-500 ease-in-out group-hover:scale-75";
+
 const ExternalLinks = () => {
   return (
     <div className="flex flex-col lg:flex-row justify-end py-12 items-center">
@@ -9,12 +17,10 @@ const ExternalLinks = () => {
         <Link
           href="https://www.booking.com/hotel/gr/bluewind-apartments-near-the-sea"
           target="_blank"
-          className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-lg font-medium
-          hover:border-4 hover:font-bold hover:bg-neutral-lightGray hover:border-primary-coral hover:scale-95 hover:transition-all hover:ease-in-out
-          focus:outline-none focus:ring-0 focus:ring-primary-coral focus:ring-opacity-50 focus:ring-offset-2 transition-all duration-300 ease-in-out"
+          className={buttonBaseStyles}
         >
           BOOK NOW
-          <div className="transition-transform duration-500 ease-in-out group-hover:scale-75">
+          <div className={logoHoverStyles}>
             <Image
               src="/images/Booking Logo.svg"
               alt="booking logo"
@@ -25,21 +31,21 @@ const ExternalLinks = () => {
           </div>
         </Link>
       </div>
+
       {/* Separator: vertical on lg, horizontal on smaller screens */}
       <div className="hidden lg:block border-l-2 border-primary h-32 p-4"></div>
       <div className="block lg:hidden border-t-2 border-primary w-full p-4"></div>
+
       {/* Airbnb Links */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="flex flex-col space-y-4">
           <Link
-            href="https://www.airbnb.gr/rooms/42484053?source_impression_id=p3_1737825240_P3C4R2jrZDklesW-"
+            href="https://www.airbnb.gr/rooms/42484053"
             target="_blank"
-            className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-lg font-medium
-              hover:border-4 hover:border-primary-coral hover:text-primary hover:font-bold hover:bg-neutral-lightGray hover:scale-95 hover:transition-all hover:ease-in-out
-              focus:outline-none focus:ring-0 focus:ring-primary-coral focus:ring-opacity-50 focus:ring-offset-2 transition-all duration-300 ease-in-out"
+            className={buttonBaseStyles}
           >
             APARTMENT A
-            <div className="transition-transform duration-500 ease-in-out group-hover:scale-75">
+            <div className={logoHoverStyles}>
               <Image
                 src="/images/airbnblogo.svg"
                 alt="airbnb logo"
@@ -50,14 +56,12 @@ const ExternalLinks = () => {
             </div>
           </Link>
           <Link
-            href="https://www.airbnb.gr/rooms/42777596?_set_bev_on_new_domain=1621165347_ZTVkMDM5YzZlMTU3&source_impression_id=p3_1621170584_CcF%2Fa1X3vFl2ID62&guests=1&adults=1"
+            href="https://www.airbnb.gr/rooms/42777596"
             target="_blank"
-            className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-lg font-medium
-              hover:border-4 hover:border-primary-coral hover:text-primary hover:font-bold hover:bg-neutral-lightGray hover:scale-95 hover:transition-all hover:ease-in-out
-              focus:outline-none focus:ring-0 focus:ring-primary-coral focus:ring-opacity-50 focus:ring-offset-2 transition-all duration-300 ease-in-out"
+            className={buttonBaseStyles}
           >
             APARTMENT D
-            <div className="transition-transform duration-500 ease-in-out group-hover:scale-75">
+            <div className={logoHoverStyles}>
               <Image
                 src="/images/airbnblogo.svg"
                 alt="airbnb logo"
@@ -70,14 +74,12 @@ const ExternalLinks = () => {
         </div>
         <div className="flex flex-col space-y-4">
           <Link
-            href="https://www.airbnb.gr/rooms/42659201?source_impression_id=p3_1737825305_P39x8L5HZgonR4Kn"
+            href="https://www.airbnb.gr/rooms/42659201"
             target="_blank"
-            className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-lg font-medium
-              hover:border-4 hover:border-primary-coral hover:text-primary hover:font-bold hover:bg-neutral-lightGray hover:scale-95 hover:transition-all hover:ease-in-out
-              focus:outline-none focus:ring-0 focus:ring-primary-coral focus:ring-opacity-50 focus:ring-offset-2 transition-all duration-300 ease-in-out"
+            className={buttonBaseStyles}
           >
             <span className="flex-grow text-center">STUDIO B</span>
-            <div className="transition-transform duration-500 ease-in-out group-hover:scale-75">
+            <div className={logoHoverStyles}>
               <Image
                 src="/images/airbnblogo.svg"
                 alt="airbnb logo"
@@ -88,14 +90,12 @@ const ExternalLinks = () => {
             </div>
           </Link>
           <Link
-            href="https://www.airbnb.com.mt/rooms/42776701?source_impression_id=p3_1737825333_P3zpllITeIVmLpa3"
+            href="https://www.airbnb.com.mt/rooms/42776701"
             target="_blank"
-            className="group flex items-center border-2 border-primary bg-neutral-white text-primary rounded-xl px-3 py-3 text-lg font-medium
-              hover:border-4 hover:border-primary-coral hover:text-primary hover:font-bold hover:bg-neutral-lightGray hover:scale-95 hover:transition-all hover:ease-in-out
-              focus:outline-none focus:ring-0 focus:ring-primary-coral focus:ring-opacity-50 focus:ring-offset-2 transition-all duration-300 ease-in-out"
+            className={buttonBaseStyles}
           >
             <span className="flex-grow text-center">STUDIO C</span>
-            <div className="transition-transform duration-500 ease-in-out group-hover:scale-75">
+            <div className={logoHoverStyles}>
               <Image
                 src="/images/airbnblogo.svg"
                 alt="airbnb logo"
